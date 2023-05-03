d4720bbcf781f245cba7bb969d5404194cae5603
Sagar7204 <128134160+Sagar7204@users.noreply.github.com>
2023-05-03 15:23:48 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,35 @@
 //input: Anitha
 //output: Aahint 
 
+#include<stdio.h>
 int main()
 {
-  return 0;
+	char i[100];
+	int j[100];
+	printf("enter string   : ");
+    scanf("%s",&i);
+
+	for(int k=0;k<7;k++)
+{
+	j[k]=i[k];
+}
+	for(int k=0;k<7;k++)                     
+	{
+		for(int l=0;l<k;l++)
+		{
+		if(j[k]<j[l])
+		  {
+			int t=j[l];
+			j[l]=j[k];
+			j[k]=t;
+	      }
+		}
+	}
+	printf("\n");
+	printf("sorted string  : ");
+for(int k=1;k<7;k++)
+{
+	printf("%c",j[k]);
+}
+	return 0;
 }
