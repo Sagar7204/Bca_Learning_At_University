3a16d444f0ccbd7f8590a014da64409faa4a8ef8
Sagar7204 <128134160+Sagar7204@users.noreply.github.com>
2023-05-03 15:12:39 +0530

    Update Assignment11.c

--- Assignment11.c
+++ Assignment11.c
@@ -1,8 +1,23 @@
 //Convert an decimal to its binary equivalent using a bitwise operator
 
+#include<stdio.h>
+
+#define size sizeof(int)*8//size in bits
 int main()
 {
-  int N = 7;
+  int N ,i,array[size];
+  scanf("%d",&N);
+  i=size-1;
   
+  while(i>=0)
+  {
+  	array[i]=N&1;
+  	i--;
+  	N>>=1;
+  }printf("binary conversion : ");
+  for(i=0;i<size;i++)
+  {
+  	printf("%d",array[i]);
+  }
   return 0;
 }
